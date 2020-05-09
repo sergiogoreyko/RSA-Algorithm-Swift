7ffa06d0db7b2fb1e338928fe25caad33781de16
sergiogoreyko <sergei_goreiko@mail.ru>
2020-05-09 17:41:29 +0300

    Class implementation of algorithm

--- Swift  RSA Algorithm/main.swift
+++ Swift  RSA Algorithm/main.swift
@@ -8,181 +8,185 @@
 
 import Foundation
 
-var p, q, t: Int
-
-var m = 0
-var e = 0
-var d = 0
-
-var flag: Bool
-var num: Int = 0
-var res: Int = 0
-
-
-func isPrime(prime: Int) -> Bool {
+class RSA {
     
-    let j = Int(sqrt(Double(prime)))
+    var p: Int = 0
+    var q: Int = 0
     
-    for i in 2...j {
-        if prime.isMultiple(of: i) {
-            return false
+    init() {}
+    
+    var m: Int {
+        return p * q
+    }
+    
+    var t: Int {
+        return ( p - 1 ) * ( q - 1 )
+    }
+    
+    var e: Int {
+        for i in 2..<t {
+            if greatestCommonDivisor(i, t) == 1 {
+                return i
+            }
         }
+        
+        return -1
     }
     
-    return true
-}
-
-func greatestCommonDivisor(_ x: Int, _ y: Int ) -> Int {
-    
-    var x1 = x
-    var y1 = y
-    
-    while x1 > 0 {
-        
-        var myTemp: Int
-        
-        myTemp = x1
-        x1 = y1 % x1
-        y1 = myTemp
-    }
-    
-    return y1
-}
-
-func calculateE(_ t: Int ) -> Int {
-
-    // Выбирается целое число e ( 1 < e < t ) // взаимно простое со значением функции Эйлера (t)
-
-    for i in 2..<t {
-        if (greatestCommonDivisor(i, t) == 1 ) {
-            return i
-        }
-    }
-    
-    return -1
-}
-
-func calculateD(_ e: Int, _ t: Int) -> Int {
-    
-    // Вычисляется число d, мультипликативно обратное к числу e по модулю φ(m), то есть число, удовлетворяющее уравнению:
-    //    d ⋅ e ≡ 1 (mod φ(m))
-    
-    var d: Int
-    var k: Int = 1
-    
-    while true {
-        k += t
-        
-        if k.isMultiple(of: e) {
-            d = k / e
-            return d
+    var d: Int {
+        var d: Int
+        var k: Int = 1
+           
+        while true {
+            k += t
+               
+            if k.isMultiple(of: e) {
+                d = k / e
+                return d
+            }
         }
     }
 }
 
-func modexp(_ base: Int, _ index_n: Int, _ modulus: Int) -> Int {
+
+extension RSA {
     
-    var c = 1
-    for _ in 1...index_n {
-        c = (c * base) % modulus
+    func isPrime(_ prime: Int) -> Bool {
+        
+        let j = Int(sqrt(Double(prime)))
+        
+        for i in 2...j {
+            if prime.isMultiple(of: i) {
+                return false
+            }
+        }
+        
+        return true
     }
-    return c
-}
-
-func encrypt() {
     
-    print("\nInsert the message to encode:")
-    
-    num = Int(readLine()!)!
-    res = modexp(num, e, m)
-    
-    print("\nRESULT: \(res)")
-}
-
-func decrypt() {
-    
-    print("\nInsert the message to decode:")
-    
-    num = Int(readLine()!)!
-    res = modexp(num, d, m)
-    
-    print("\nRESULT: \(res)")
-}
-
-print("Welcome to RSA program\n")
-    
-// Cоздание открытого и секретного ключей
-    
-// 1. Выбираются два различных случайных простых числа p и q
-    
-repeat {
-    
-    print("Enter a Prime number  p :")
+    func greatestCommonDivisor(_ x: Int, _ y: Int ) -> Int {
         
-    p = Int(readLine()!)!
-    flag = isPrime(prime: p)
+        var x1 = x
+        var y1 = y
         
-    if !flag {
+        while x1 > 0 {
             
-        print("\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n")
-    }
-        
-} while !flag
-
-repeat {
-    
-    print("Enter a Prime number  q :")
-        
-    q = Int(readLine()!)!
-    flag = isPrime(prime: q)
-        
-    if !flag {
+            var myTemp: Int
             
-        print("\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n")
-    }
+            myTemp = x1
+            x1 = y1 % x1
+            y1 = myTemp
+        }
         
-} while !flag
+        return y1
+    }
     
-// 2. Вычисляется их произведение m = p ⋅ q, которое называется модулем.
-
-m = p * q
-print("\nResult of computing n = p*q = \(m)")
+    func modexp(_ base: Int, _ index_n: Int, _ modulus: Int) -> Int {
+        
+        var c = 1
+        for _ in 1...index_n {
+            c = (c * base) % modulus
+        }
+        return c
+    }
     
-// 3. Вычисляется значение функции Эйлера от числа m: φ(m) = t = (p−1)⋅(q−1)
+    func encrypt() {
+        
+        print("\nInsert the message to encode:")
+        
+        let num = Int(readLine()!)!
+        let res = modexp(num, e, m)
+        
+        print("\nRESULT: \(res)")
+    }
 
-t = ( p - 1 ) * ( q - 1 )
-print("Result of computing Euler's totient function: t = \(t)")
+    func decrypt() {
+        
+        print("\nInsert the message to decode:")
+        
+        let num = Int(readLine()!)!
+        let res = modexp(num, d, m)
+        
+        print("\nRESULT: \(res)")
+    }
     
-// 4. Выбирается целое число e ( 1 < e < φ(m) ), взаимно простое с t
-//      Число e называется открытой экспонентой
+    func buildRSA() {
+        
+        print("Welcome to RSA program\n")
+        
+        // 1. Choose two different random primes p and q.
+        
+        repeat {
+                
+            print("Enter a Prime number  p :")
+                    
+            p = Int(readLine()!)!
+                    
+            if !isPrime(p) {
+                        
+                print("\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n")
+            }
+                    
+        } while !isPrime(p)
+        
+        repeat {
+                
+            print("Enter a Prime number  p :")
+                    
+            q = Int(readLine()!)!
+                    
+            if !isPrime(q) {
+                        
+                print("\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n")
+            }
+                    
+        } while !isPrime(q)
+        
+        // 2. Calculate their multiplication m = p ⋅ q, which is called the module.
+        
+        print("\nResult of computing n = p*q = \(m)")
+        
+        // 3. Calculate the value of the Euler's function: φ(m) = t = (p−1)⋅(q−1).
+        
+        print("Result of computing Euler's function: t = \(t)")
+        
+        // 4. Choose an integer e ( 1 < e < φ(m) ), coprime to t, called the open exponent.
 
-e = calculateE(t)
+        print("Result of computing e = \(e)")
+            
+        // 5. Calculate the number d that is multiplicatively inverse to the number e modulo φ(m), that is, a number satisfying the equation:
+        //    d ⋅ e ≡ 1 (mod φ(m)).
+
+        print("Result of computing d = \(d)")
+            
+        // 6. The pair {m, e} is published as an RSA public key.
+        
+        print("\nRSA public key is (m = \(m)), (e = \(e))")
+
+        // 7. The pair {m, d} plays the role of the RSA private key and is kept secret.
+        
+        print("RSA private key is (m = \(m)), (d = \(d))\n")
+    }
     
-// 5. Вычисляется число d, мультипликативно обратное к числу e по модулю φ(m), то есть число, удовлетворяющее уравнению:
-//    d ⋅ e ≡ 1 (mod φ(m))
+    func chooseCommand() {
+        var v: Int
+        print("Choose command:\n1. Encrypt\n2. Decrypt\n-------------------")
 
-d = calculateD(e, t)
-    
-// 6. Пара {m, e} публикуется в качестве открытого ключа RSA
-
-print("\nRSA public key is (m = \(m)), (e = \(e))")
-    
-// 7. Пара {m, d} играет роль закрытого ключа RSA и держится в секрете
-
-print("RSA private key is (m = \(m)), (d = \(d))\n")
-
-var v: Int
-print("Choose command:\n1. Encrypt\n2. Decrypt")
-print("------------------- ")
-
-v = Int(readLine()!)!
-switch v {
-    case 1:
-        encrypt()
-    case 2:
-        decrypt()
-    default:
-        print("Error")
+        v = Int(readLine()!)!
+        switch v {
+            case 1:
+                self.encrypt()
+            case 2:
+                self.decrypt()
+            default:
+                print("Error")
+        }
+    }
 }
+
+
+let rsa = RSA()
+rsa.buildRSA()
+rsa.chooseCommand()
 
 
